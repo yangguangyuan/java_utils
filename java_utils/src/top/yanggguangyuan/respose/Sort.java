818ebbe02bb27ce2e687d26c736bcf7fe5f1475c
yangguangyuan <nmgyangguangyuan@163.com>
2017-04-24 16:16:49 +0800

    递归父类排序分类树

---
+++ java_utils/src/top/yanggguangyuan/respose/Sort.java
@@ -0,0 +1,29 @@
+package top.yanggguangyuan.respose;
+
+/**
+ * @ClassName: Sort 
+ * @Description: TODO() 
+ * @author yangguangyuan
+ * @date 2017年4月24日 下午3:34:14 
+ *
+ */
+public class Sort {
+	
+	// 递归父类排序分类树
+//	private List<Role> recursivRoleTreeList(List<Role> allRoleList, Role p,
+//			List<Role> temp) {
+//		if (temp == null) {
+//			temp = new ArrayList<Role>();
+//		}
+//		for (Role role : allRoleList) {
+//			Role parent = role.getParent();
+//			if (((p == null && parent == null) || (role != null && parent == p))) {
+//				temp.add(role);
+//				if (role.getChildren() != null && role.getChildren().size() > 0) {
+//					recursivRoleTreeList(allRoleList, role, temp);
+//				}
+//			}
+//		}
+//		return temp;
+//	}
+}
